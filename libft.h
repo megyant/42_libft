171e7e5477ba73f6f62f12b06fa7c333a8d44207
megyant <megyant@gmail.com>
2025-10-17 15:38:51 +0100

    added files

--- libft.h
+++ libft.h
@@ -4,4 +4,16 @@
 
 # include <stddef.h>
 
+// chars
+int	ft_isalnum(char c);
+int	ft_isalpha(char c);
+int	ft_isascii(char c);
+int	ft_isdigit(char c);
+int	ft_isprint(char c);
+
+// strings
+int	ft_strlen(char *str);
+
+// Memmory management
+void	*ft_memset(void *s, int c, size_t n);
 #endif
